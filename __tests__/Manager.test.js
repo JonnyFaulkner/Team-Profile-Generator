f98f5185eb95c469203a80eaadca874ee80597ad
Jonathan Faulkner <83264372+JonnyFaulkner@users.noreply.github.com>
2021-07-18 11:50:15 -0500

    created manager constructor

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,16 @@
+const Manager = require('../lib/Manager');
+
+test('creates manager object', () => {
+    const manager = new Manager('John Doe', '2', 'something@something.com', '12');
+
+    expect(manager.name).toBe('John Doe');
+    expect(manager.id).toBe('2');
+    expect(manager.email).toBe('something@something.com');
+    expect(manager.officeNumber).toBe('12');
+});
+
+test('gets managers role', () => {
+    const manager = new Manager('John Doe', '2', 'something@something.com', '12');
+
+    expect(manager.getRole()).toEqual(expect.stringContaining('Manager'))
+})
